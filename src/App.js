dda535760be423dd58bf908bfc20171d3d42b0a5
vaniammu@123 <vanimanoj1997@gmail.com>
2022-05-18 00:25:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Viewuser from './component/Viewuser';
 
 function App() {
   return (
     <div>
 
-      
+      <Viewuser/>
     </div>
   );
 }
